fa2a27d7e97429d95a8cd8038e2cb546272e5455
ricardombrodriguez <ricardorodriguez@ua.pt>
2021-07-13 23:56:54 +0100

    grid done + bombs/default cells

---
+++ Cell.py
@@ -0,0 +1,14 @@
+class Cell:
+
+    def __init__(self,type):
+        self.bomb = True if type == "BOMB" else False
+        self.flag = False
+        self.clicked = False
+        self.number = None
+    
+    def click(self):
+        pass
+
+    def flagged(self):
+        pass
+
--- Game.py
+++ Game.py
@@ -1,23 +1,60 @@
-from Const import WIDTH,HEIGHT
-import pygame, sys
+from Const import *
+from Cell import Cell
+import pygame, sys, random
 
 class Game:
 
     def __init__(self):
         pygame.init()
-
+        self.running = True
         self.canvas = pygame.display.set_mode((WIDTH,HEIGHT))
-        self.canvas.fill((255,255,255))
+        self.canvas.fill(WHITE)
         pygame.display.set_caption("Minesweeper")
         pygame.display.flip()
-        self.running = True
+        self.createCells()
 
     def run(self):
         while self.running:
-            #do stuff
-            continue
+            self.events()
         pygame.quit()
         sys.exit(0)
+
+    # Create cells
+    def createCells(self):
+        self.grid = [[None for col in range(COL_CELLS)] for row in range(ROW_CELLS)] 
+        self.createBombs()
+        self.createDefault()
+        self.drawCanvas()
+
+    # Creating bombs
+    def createBombs(self):
+        num_cells = ROW_CELLS * COL_CELLS
+        num_bombs = NUM_BOMBS
+        while num_bombs != 0:
+            randnum = random.randint(0,num_cells-1)
+            if self.grid[randnum//COL_CELLS][randnum%COL_CELLS] is None:
+                cell_bomb = Cell("BOMB")
+                self.grid[randnum//COL_CELLS][randnum%COL_CELLS] = cell_bomb
+                num_bombs -= 1
+
+    # Creating default/non-bomb cells
+    def createDefault(self):
+        for row in range(0,ROW_CELLS):
+            for column in range(0,COL_CELLS):
+                if self.grid[row][column] is None:
+                    cell = Cell("DEFAULT")
+                    self.grid[row][column] = cell
+        print("done create default")
+
+    # Draw canvas/grid
+    def drawCanvas(self):
+        pygame.draw.rect(self.canvas, BLACK, (GRID_POS[0], GRID_POS[1], GRID_WIDTH, GRID_HEIGHT), 2)
+        for x in range(COL_CELLS):
+            pygame.draw.line(self.canvas, BLACK, (GRID_POS[0]+(x*CELL_SIZE), GRID_POS[1]), (GRID_POS[0]+(x*CELL_SIZE), GRID_POS[1]+GRID_HEIGHT), 1)
+        for y in range(ROW_CELLS):
+            pygame.draw.line(self.canvas, BLACK, (GRID_POS[0], GRID_POS[1]+(y*CELL_SIZE)), (GRID_POS[0]+GRID_WIDTH, GRID_POS[1]+(y*CELL_SIZE)), 1)
+        pygame.display.flip()
+        print("canvas draw done")
 
     def events(self):
         for event in pygame.event.get():
